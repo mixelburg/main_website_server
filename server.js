d57aeb566f468eb96f44c9af4e822219628a0fbb
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-05-13 00:14:09 +0300

    bugfix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb")
 const Express = require("express")
+const path = require("path")
 const config = require("./config.json")
 const cors = require('cors');
 
@@ -25,8 +26,15 @@ async function connectDB() {
         const app = Express()
 
         app.use(cors({
-            origin: 'http://192.168.1.88'
+            origin: '*'
         }));
+
+        app.use(Express.static(path.join(__dirname, 'build')));
+
+        app.get('/', function (req, res) {
+            res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        });
+
 
         app.get('/projects', function(req, res){
             getData(projects).then(data => {
