2a37e414f294d4e3b3cb005c2767a4851f39e031
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-05-22 17:01:45 +0300

    recaptcha support added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path")
 const config = require("./config.json")
 const cors = require('cors');
 const https = require('https');
+const fetch = require('node-fetch');
 
 const privateKey  = fs.readFileSync('/etc/letsencrypt/live/mixelburg.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/mixelburg.com/fullchain.pem', 'utf8');
@@ -12,7 +13,7 @@ const credentials = {key: privateKey, cert: certificate};
 
 const uri = config["db_uri"]
 const port = config["listen_port"]
-
+const SECRET_KEY = config["SECRET_KEY"];
 
 async function getData(collection) {
     let result = await collection.find()
@@ -66,6 +67,13 @@ async function connectDB() {
             })
         });
 
+        // verify reCAPTCHA response
+        app.post('/verify', (req, res) => {
+            const VERIFY_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${req.body['g-recaptcha-response']}`;
+            return fetch(VERIFY_URL, { method: 'POST' })
+                .then(res => res.json())
+                .then(json => res.send(json));
+        });
 
         const httpsServer = https.createServer(credentials, app);
         httpsServer.listen(port)
